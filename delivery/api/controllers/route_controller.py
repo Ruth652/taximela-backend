2a395cd02ffbb7430e391af181f1115ade2d1776
Ruth Ereso <ruthfian74@gmail.com>
2026-02-09 20:10:41 +0300

    feat: update /routes/plan response structure and add sorting options

--- delivery/api/controllers/route_controller.py
+++ delivery/api/controllers/route_controller.py
@@ -1,4 +1,4 @@
-from usecases.route_usecase import get_trip_plan
+from usecases.route_usecase import get_transformed_trip_plan
 
 async def plan_trip_controller(request_data):
-    return await get_trip_plan(request_data)
+    return await get_transformed_trip_plan(request_data)
--- infrastructure/route_services.py
+++ infrastructure/route_services.py
@@ -1,57 +1,11 @@
-# # infrastructure/example_services.py
-
-# import httpx
-
-# OTP_URL = "http://localhost:8080/otp/gtfs/v1"
-
-# async def fetch_route_from_otp(from_lat, from_lon, to_lat, to_lon, date, time):
-#     query = {
-#         "query": """
-#         {
-#           plan(
-#             from: {lat: %f, lon: %f},
-#             to: {lat: %f, lon: %f},
-#             date: "%s",
-#             time: "%s",
-            
-#           ) {
-#             itineraries {
-#               duration
-#               walkDistance
-#               numberOfTransfers
-#               legs {
-#                 mode
-#                 startTime
-#                 endTime
-#                 from { name lat lon }
-#                 to { name lat lon }
-#                 route { shortName longName }
-#                 legGeometry {
-#                   points
-#                   length
-#                 }
-#               }
-#             }
-#           }
-#         }
-#         """ % (from_lat, from_lon, to_lat, to_lon, date, time)
-#     }
-
-#     timeout = httpx.Timeout(60.0, connect=10.0)  
-#     async with httpx.AsyncClient() as client:
-#         response = await client.post(OTP_URL, json=query)
-#         response.raise_for_status()
-#         return response.json()
-
-
-
-# infrastructure/example_services.py
 
 import httpx
+import os
 
 OTP_URL = "http://localhost:8080/otp/gtfs/v1"
+# OTP_URL = os.getenv("OTP_BASE_URL")
 
-async def fetch_route_from_otp(from_lat, from_lon, to_lat, to_lon, date, time):
+async def fetch_route_from_otp(from_lat, from_lon, to_lat, to_lon, time = "06:00:00"):
     query = {
         "query": f"""
         {{
@@ -80,6 +34,7 @@ async def fetch_route_from_otp(from_lat, from_lon, to_lat, to_lon, date, time):
                 from {{ name lat lon }}
                 to {{ name lat lon }}
                 route {{ shortName longName }}
+                distance
                 legGeometry {{
                 points
                 length
@@ -101,3 +56,7 @@ async def fetch_route_from_otp(from_lat, from_lon, to_lat, to_lon, date, time):
         except httpx.HTTPError as e:
             # Catch network errors, timeouts, and HTTP status errors
             return {"error": str(e)}
+
+
+
+   
