647b8fe1184d17ff007d3021091577fa531a283b
Ruth Ereso <ruthfian74@gmail.com>
2026-02-12 15:47:15 +0300

    feat(users): create /api/users endpoint for first-time Firebase users in database

---
+++ repository/user_repository.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session
+from domain.user_model import User
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+    
+
+    def get_user_by_id(self, user_id):
+         return self.db.query(User).filter(User.id == user_id).first()
+
+    def get_user_by_email(self, email: str):
+        return self.db.query(User).filter(User.email == email).first()
+
+    def create_user(self, email, full_name=None, preferred_language="en"):
+        user = User(
+            email= email,
+            full_name = full_name,
+            preferred_language = preferred_language,
+
+        )
+        
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+        
